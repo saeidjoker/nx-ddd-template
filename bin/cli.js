2b629430aa41e102b5ecbf244f7ecc300b6b422e
Saeed Farahi <saeed.farahi@eversports.com>
2022-06-26 19:23:20 +0200

    first draft ready

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process')
 
-const runCommand = command => {
+const runCommand = (command) => {
   try {
-    execSync(command, {stdio: 'inherit'})
+    execSync(command, { stdio: 'inherit' })
+    return true
   } catch (error) {
-    
+    console.log(`Failed to execute ${command}`, e)
+    return false
   }
 }
 
@@ -14,4 +16,13 @@ const gitCheckoutCommand = `git clone --depth 1 git@github.com:saeidjoker/nx-ddd
 const installDepsCommand = `cd ${repositoryName} && npm i`
 
 console.log(`Cloning the repository with name ${repositoryName}`)
-const checkedOut = 
+const checkedOut = runCommand(gitCheckoutCommand)
+if (!checkedOut) process.exit(-1)
+
+console.log(`Installing dependencies for ${repositoryName}`)
+const installedDeps = runCommand(installDepsCommand)
+if (!installedDeps) process.exit(-1)
+
+console.log("Congratulations! You're API app and Shared library are ready.")
+console.log('Follow the following commands to start')
+console.log(`cd ${repositoryName} && npm serve api`)
