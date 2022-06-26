643b74d0f419e6e88a2efbd69c8bdb1614d8d08d
Saeed Farahi <saeed.farahi@eversports.com>
2022-06-26 20:54:12 +0200

    before replacing text in files

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process')
+const { program } = require('commander')
+const replaceInFile = require('replace-in-file')
+
+let inquirer
+import('inquirer').then((module) => {
+  inquirer = module.default
+  main()
+})
 
 const runCommand = (command) => {
   try {
@@ -11,18 +19,78 @@ const runCommand = (command) => {
   }
 }
 
-const repositoryName = process.argv[2]
-const gitCheckoutCommand = `git clone --depth 1 git@github.com:saeidjoker/nx-ddd-template.git ${repositoryName}`
-const installDepsCommand = `cd ${repositoryName} && npm i`
+const isYes = (v) => (typeof v === 'string' && v.toString().toLowerCase() === 'y') || (typeof v === 'boolean' && v)
 
-console.log(`Cloning the repository with name ${repositoryName}`)
-const checkedOut = runCommand(gitCheckoutCommand)
-if (!checkedOut) process.exit(-1)
+function main() {
+  const interactiveMode = () => {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Repository name:',
+        },
+        {
+          type: 'confirm',
+          name: 'nvm',
+          message: 'Use NVM?',
+          default: true,
+        },
+        {
+          type: 'confirm',
+          name: 'husky',
+          message: 'Use husky?',
+          default: true,
+        },
+      ])
+      .then((answers) => {
+        answers.nvm = answers.nvm ? 'y' : 'n'
+        answers.husky = answers.husky ? 'y' : 'n'
+        install(answers)
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+      })
+  }
 
-console.log(`Installing dependencies for ${repositoryName}`)
-const installedDeps = runCommand(installDepsCommand)
-if (!installedDeps) process.exit(-1)
+  const install = (params) => {
+    console.log(params)
+    const { nvm, husky, name } = params
 
-console.log("Congratulations! You're API app and Shared library are ready.")
-console.log('Follow the following commands to start')
-console.log(`cd ${repositoryName} && nx serve api`)
+    
+  }
+
+  const options = program
+    .option('--name <name>', 'Repository name')
+    .option('--nvm <Y/n>', 'Use NVM', 'y')
+    .option('--husky <Y/n>', 'Use husky', 'y')
+    .parse(process.argv)
+    .opts()
+
+  const hasAllOptions = options.name && options.nvm && options.husky
+  if (!hasAllOptions) {
+    interactiveMode()
+  } else {
+    install(options)
+  }
+
+  // const repositoryName = process.argv[2]
+  // const gitCheckoutCommand = `git clone --depth 1 git@github.com:saeidjoker/nx-ddd-template.git ${repositoryName}`
+  // const installDepsCommand = `cd ${repositoryName} && npm i`
+
+  // console.log(`Cloning the repository with name ${repositoryName}`)
+  // const checkedOut = runCommand(gitCheckoutCommand)
+  // if (!checkedOut) process.exit(-1)
+
+  // console.log(`Installing dependencies for ${repositoryName}`)
+  // const installedDeps = runCommand(installDepsCommand)
+  // if (!installedDeps) process.exit(-1)
+
+  // console.log("Congratulations! You're API app and Shared library are ready.")
+  // console.log('Follow the following commands to start')
+  // console.log(`cd ${repositoryName} && nx serve api`)
+}
